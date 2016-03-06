efcd5529e3cacb866957e08dbf0b8f8b5e4a4133
kzsh <kzsh@kzsh.dev>
2016-03-06 13:24:40 -0500

    factoring gulp tasks.  Add concat+minify

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,40 @@ var config = require('./config/config'),
     gulp = require('gulp'),
     babel = require('gulp-babel'),
     jshint = require('gulp-jshint'),
-    notify = require('gulp-notify');
+    uglify = require('gulp-uglify'),
+    notify = require('gulp-notify'),
+    concat = require('gulp-concat'),
+    merge = require('merge-stream');
 
 gulp.task('default', function() {
-    gulp.start('plugins');
+    gulp.start('build');
 });
 
-gulp.task('plugins', function() {
-  return gulp.src('plugins/**/*.js')
+gulp.task('build', function() {
+  var plugins = gulp.src('plugins/**/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
     .pipe(babel())
+    .pipe(gulp.dest('build/'));
+
+  var rot = gulp.src('node_modules/rot-js/lib/rot.js')
+    .pipe(gulp.dest('build/'));
+    return merge(plugins, rot)
+      .pipe(notify({ message: 'build complete' }));
+});
+
+gulp.task('dist', ['build'], function () {
+    return gulp.src('build/**/*.js')
+      .pipe(concat('dist.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/'))
+      .pipe(notify({ message: 'dist complete' }));
+});
+
+gulp.task('export', ['dist'], function () {
+  gulp.src('dist/**/*.js')
     .pipe(gulp.dest(`${config.serverPath}/scriptcraft/plugins/ROT_js`))
-    .pipe(notify({ message: 'Plugins task complete' }));
+    .pipe(notify({ message: 'export complete' }));
 });
 
 gulp.task('watch', function() {
